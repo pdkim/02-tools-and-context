87c0f8f61bba7a3a1e4b50a89b445bf213030ecb
Phillip Kim <phildkim@gmail.com>
2018-05-17 20:54:33 -0700

    ready for coding

---
+++ lab-phil/__test__/list.test.js
@@ -0,0 +1,7 @@
+const list = require('../lib/list.js');
+
+describe('testing', () => {
+  it('should be', () => {
+    expect(list.test()).toBeNull();
+  });
+});
---
+++ lab-phil/lib/list.js
@@ -0,0 +1,5 @@
+module.exports = list = {};
+
+list.test = function() {
+  return null;
+};
