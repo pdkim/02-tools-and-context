55f0cba0c1a549c345ac5340de5d9e998c73fe8e
Phillip Kim <phildkim@gmail.com>
2018-05-19 18:21:44 -0700

    working

--- lab-phil/__test__/list.test.js
+++ lab-phil/__test__/list.test.js
@@ -1,7 +1,113 @@
-const list = require('../lib/list.js');
+'use strict';
 
-describe('testing', () => {
-  it('should be', () => {
-    expect(list.test()).toBeNull();
+let List = require('../lib/list.js');
+
+describe('list module', () => {
+  //push
+  xit('should add to empty list', () => {
+    let list = new List();
+    let expected = 'something';
+    list.push('something');
+    let actual = list[0];
+    expect(actual).toBe(expected);
+    expect(list.length).toBe(1);
+  });
+
+  xit('should add to single item list', () => {
+    let list = new List();
+    let expected = 'else';
+    list.push('something');
+    list.push('else');
+    let actual = list[1];
+    expect(actual).toBe(expected);
+    expect(list.length).toBe(2);
+  });
+
+  //pop
+  xit('should remove last item in list', () => {
+    let list = new List();
+    list.push(1);
+    list.push(2);
+    list.pop();
+    let expected = 1;
+    let actual = list[0];
+    expect(actual).toBe(expected);
+    expect(list.length).toBe(1);
+  });
+
+  xit('should return the last item removed', () => {
+    let list = new List();
+    list.push(1);
+    list.push(2);
+    let actual = list.pop();
+    list.pop;
+    let expected = 2;
+    expect(actual).toBe(expected);
+  });
+
+  xit('should return undefined if length is 0.', () => {
+    let list = new List();
+    list.push(1);
+    let expected = undefined;
+    list.pop();
+    expect(list.pop()).toBe(expected);
+    expect(list.length).toBe(0);
+  });
+  
+
+  //forEach
+  xit('should iterate through each item in the array', () => {
+    let list = new List;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let newArr = [];
+    list.forEach(item => newArr.push(item));
+    let actual = newArr;
+    let expected = list.toArray();
+    expect(actual).toEqual(expected);
+  });
+
+  xit('should return undefined', () => {
+    let list = new List;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let actual = list.forEach(item => console.log(item));
+    let expected = undefined;
+    expect(actual).toEqual(expected);
+  });
+
+
+  //map
+  it('by create a new array affecting all values', () => {
+    let list = new List;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let actual = list.toArray().map(item => item * 5);
+    let expected = [5, 10, 15];
+    expect(actual).toEqual(expected);
+  });
+
+  //filter
+  xit('by making a new array that returns only even numbers', () => {
+    expect(list.filter()).toBeNull();
+  });
+
+  //reduce
+  xit('by reducing all the elements in the array into a single result', () => {
+    expect(list.reduce()).toBeNull();
   });
 });
+
+//let source = [1, 2,3];
+//let list = new List(source);
+//list.pop();
+//source.pop();
+//expect(list.toArray()).toEqual(source);
+
+//let list = new List('apples', 'bananas');
+
+//let babyfood = list.map(fruits => fruit + 'sauce');
+//expect(babyfoods.toArray()).toEqual(['apples sauce', 'bananas sauce']);
--- lab-phil/lib/list.js
+++ lab-phil/lib/list.js
@@ -1,5 +1,81 @@
-module.exports = list = {};
+'use strict';
 
-list.test = function() {
-  return null;
-};
+class List {
+
+  constructor(array = []) {
+    this.length = array.length;
+
+    //populate list with item(s) in array
+    for(let i = 0; i < array.length; i++) {
+      this[i] = array[i];
+    }
+  }
+
+  push(item) {
+    this[this.length] = item;
+    this.length++;
+  }
+
+  pop() {
+    let last = this[this.length-1];
+    this.length--;
+    if(this.length < 0) {
+      this.length = 0;
+    }
+    if(this.length === 0) {
+      last = undefined;
+    }
+    return last;
+  }
+
+  forEach(func) {
+    for(let i = 0; i < this.length; i++) {
+      func(this[i]);
+    }
+    return undefined;
+  }
+
+  map(func) {
+    for(let i = 0; i < this.length; i++) {
+      var arr = [];
+      arr[i] = func(this[i]);
+    }
+    return arr;
+  }
+
+  
+  toArray() {
+    let array = [];
+    
+    //start at 0, end at length
+
+    for(let i = 0; i < this.length; i++) {
+      array[i] = this[i];
+    }
+    return array;
+  }
+
+}
+// list.map = function(arr) {
+//   let newArr = [];
+//   for(let i = 0; i < arr.length; i++) {
+//     newArr[i] = arr[i].toUpperCase();
+//   }
+//   return newArr;
+// };
+
+// list.filter = function(arr) {
+//   let evenOnly = [];
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] % 2 === 0) {
+//       evenOnly[evenOnly.length] = arr[i];
+//     }
+//   }
+//   return evenOnly;
+// };
+
+// list.reduce = function() {
+//   return null;
+// };
+
+module.exports = List;
